5d6f43226778f4fb08e300d0da223d932587861c
Alex Bordovskih <alex.bordovskih@firstlinesoftware.com>
2020-11-04 03:19:54 +0300

    footer, logo files renamed on ts

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 const logoLetters = ['W', 'T', 'W'];
@@ -14,10 +13,6 @@ const Logo = props => {
             {logoLettersRender}
         </Link>
     </div>
-};
-
-Logo.propTypes = {
-    modifier: PropTypes.string
 };
 
 export default Logo;
